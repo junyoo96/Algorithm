e0f3c0c38e840a7e94440b0bad4bd143b612ca08
junyoo96 <junyoo96@gmail.com>
2021-03-21 09:47:52 +0900

    Update 주식가격.py

--- 프로그래머스문제/스택_큐/2단계/주식가격.py
+++ 프로그래머스문제/스택_큐/2단계/주식가격.py
@@ -23,34 +23,6 @@ def solution(prices):
     
     return answer
 
-#처음 풀이 
-def solution(prices):
-    answer = []
-    
-    for i in range(len(prices)-1):
-        cnt=0
-        for j in range(i+1,len(prices)):
-            cnt+=1
-            if prices[i]>prices[j]:
-                break
-        answer.append(cnt)
-    
-    answer.append(0)
-    
-    return answer
-  
-#비슷한 풀이 중 깔끔한 코드
-def solution(prices):
-  answer = [0] * len(prices)
-  for i in range(len(prices)):
-      for j in range(i+1, len(prices)):
-          if prices[i] <= prices[j]:
-              answer[i] += 1
-          else:
-              answer[i] += 1
-              break
-  return answer
-  
 #베스트 코드
     #dequeue 사용
    
@@ -71,3 +43,20 @@ def solution(prices):
         answer.append(count)
 
     return answer
+
+#처음 풀이 
+def solution(prices):
+    answer = []
+    
+    for i in range(len(prices)-1):
+        cnt=0
+        for j in range(i+1,len(prices)):
+            cnt+=1
+            if prices[i]>prices[j]:
+                break
+        answer.append(cnt)
+    
+    answer.append(0)
+    
+    return answer
+    
