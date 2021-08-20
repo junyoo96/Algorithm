9516d176e9db2c37b3c1aaee6524d85c6ab6dff1
junyoo96 <junyoo96@gmail.com>
2021-08-20 15:16:51 +0900

    Update 프린터.py

--- 프로그래머스문제/스택_큐/2단계/프린터.py
+++ 프로그래머스문제/스택_큐/2단계/프린터.py
@@ -1,26 +1,26 @@
-# 2ㅅㅣㄱㅏㄴ ㅊㅗㄱㅗㅏ 
+# 2 hour 
 
 def solution(priorities, location):
-    answer = 
-    priorities = deque(priorities)
+    answer = 0 
+    priorities = deque(priorities) # convert array into deque to make add and pop faster
     m = max(priorities)
-    while priorities:
-        doc = priorities.popleft()
+    while priorities: # while document exists
+        doc = priorities.popleft() # O(1)
         
-        if doc == m:
-            answer += 1
-            
-            if location == 0:
+        if doc == m: # if current document has max priority
+            answer += 1 
+             
+            if location == 0: # if current document is up to print 
                 break 
             else:
-                location -= 1
-            m = max(priorities)
-        else:
-            priorities.append(doc)
+                location -= 1 # move location 
+            m = max(priorities) # update max priority
+        else: 
+            priorities.append(doc) # append current document 
             
-            if location == 0:
+            if location == 0: # update location into end of deque
                 location = len(priorities)-1
             else:
-                location -= 1
+                location -= 1 # move location forward
     
     return answer
