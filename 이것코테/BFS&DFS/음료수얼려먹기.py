66593c5ab417b07e4d9035fb919b43e19a1fad5f
root <root@DESKTOP-9JSPC7N.localdomain>
2022-03-23 13:14:51 +0900

    cadd 음료수얼려먹기.py

---
+++ 이것코테/BFS&DFS/음료수얼려먹기.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+# N, M을 공백을 기준으로 구분하여 입력 받기
+n, m = map(int, input.split())
+
+# 2차원 리스트의 맵 정보 입력 받기
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+#DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
+def dfs(x,y):
+    #주어진 좌표 범위를 벗어나느 경우에는 즉시 종료
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    #현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        #해당 노드 방문 처리
+        graph[x][y] = 1
+        #상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        return True
+    return False
+
+#모든 노드에 대해 음료수 채우기
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i,j):
+            result += 1 #한번에 만들 수 있는 아이스크림 개수 계산
+
+print(result)
+
