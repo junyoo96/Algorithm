6dad838c09b4278b416ec3fd13b353e87cab1ebe
root <root@DESKTOP-9JSPC7N.localdomain>
2022-05-16 12:45:59 +0900

    add 3단계 그리디 모험가길드 곱하기혹은더하기

---
+++ 이것코테/그리디(Greedy)/3단계/곱하기혹은더하기.py
@@ -0,0 +1,19 @@
+#12:34~
+
+# 숫자로만 이루어진 문자열 S
+# 가장 큰 수 구하기
+
+# 최적해 방안 : 기본적으로 곱하기가 우선이지만, 0이나 1인 숫자가 계산에 들어가는 경우 +가 우선됨
+
+numbers = list(map(int, input())) # 각 자리 숫자 리스트로 변환
+result = numbers[0] # 최대 값
+
+for i in range(1, len(numbers)):
+    # 더하기 수행하는 경우
+    if result <= 1 or numbers[i] <= 1:
+        result += numbers[i]
+    # 곱하기 수행하는 경우
+    else:
+        result *= numbers[i]
+
+print(result)
