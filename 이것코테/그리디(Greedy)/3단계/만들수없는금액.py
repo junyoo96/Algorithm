c2d2db125b994a04b3f0a78b9f4019628bc214e6
root <root@DESKTOP-9JSPC7N.localdomain>
2022-05-17 14:52:03 +0900

    add 3단계 그리디 a만들수없는금액

---
+++ 이것코테/그리디(Greedy)/3단계/만들수없는금액.py
@@ -0,0 +1,51 @@
+#12:56~
+
+n = int(input())
+coins = list(map(int, input().split()))
+coins.sort() # 동전 오름차순 정렬
+
+# 현재 상태를 1 ~ target-1 까지의 모든 금액을 만들 수 있는 상태라 가정
+
+# 만들고자 하는 금액을 1원부터 시작
+target = 1
+for coin in coins:
+    # 매번 target인 금액도 만들 수 있는지 체크하기
+        # 현재 코인이 만들고자 하는 금액보다 클 경우, 만들고자 하는 금액을 만드는 방법은 없기 때문에, 해당 금액이 만들 수 없는 최소 금액이 됨
+    if target < coin:
+        break
+    # 해당 금액을 만들 수 있다면, target의 값을 업데이트
+        # 현재 코인 만들고자 하는 금액보다 적은 경우, (만들고자 하는 금액 + 현재 코인) 까지 만들 수 있다는 의미가 되기 때문에 만들고자 하는 금액을 높임
+    target += coin
+
+print(target)
+
+
+
+#========================================================================
+# from itertools import combinations # 주의 - itertools.combinations
+#
+# answer = 0
+#
+# n = int(input())
+# coins = list(map(int, input().split()))
+# # 동전 리스트 오름차순 정렬
+# coins.sort()
+#
+# # 함수
+# # 조합 라이브러리 푸는 문제 선택 개수 늘려가면서 - 완전탐색
+# def check_possible(money):
+#     for i in range(1, n + 1):
+#         for candidate in combinations(coins, i):
+#             if sum(list(candidate)) == money:
+#                 return True
+#     return False
+#
+# # 1원부터 1,000,000 * n 까지 반복하면서
+# for i in range(1, coins[-1] * n):
+#     # 함수 - 동전 리스트로 조합해서 현재 금액 만들 수 없다면
+#     if not check_possible(i):
+#         answer = i
+#         break
+#
+# # 현재 최소 금액 출력
+# print(answer)
