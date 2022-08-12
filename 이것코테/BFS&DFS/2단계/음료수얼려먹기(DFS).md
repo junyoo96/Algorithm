d8af57878b14f5ab1e87268c222099abccd23e78
root <root@DESKTOP-9JSPC7N.localdomain>
2022-08-12 15:38:14 +0900

    Update 음료수얼려먹기

--- 이것코테/BFS&DFS/2단계/음료수얼려먹기(DFS).md
+++ 이것코테/BFS&DFS/2단계/음료수얼려먹기(DFS).md
@@ -6,4 +6,13 @@
 ![캡처](https://user-images.githubusercontent.com/25860354/160047413-21d58473-fd6d-4e75-a5ae-29b322ff3afd.JPG)
 - 좌표 범위 문제 경우 
   - 주어진 좌표 범위 벗어나는지 확인 
-  - 상, 하, 좌, 우 이동 = fs함수 재귀적으로 호출하는 방식
+  - 상,하,좌,우 이동 = fs함수 재귀적으로 호출하는 방식
+
+# 주의 코드 개념 
+- dfs에서 방문가능한지를 True 또는 False로 리턴
+
+![img.png](캡처/img.png)
+
+- 주어진 좌표 범위 벗어나는지 확인 
+
+![img_1.png](캡처/img_1.png)
--- 이것코테/BFS&DFS/2단계/음료수얼려먹기(DFS).py
+++ 이것코테/BFS&DFS/2단계/음료수얼려먹기(DFS).py
@@ -8,29 +8,28 @@ graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
-#DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
+# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
 def dfs(x,y):
-    #주어진 좌표 범위를 벗어나는 경우에는 즉시 종료
+    # 주의 - 주어진 좌표 범위를 벗어나는 경우에는 즉시 종료
     if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
-    #현재 노드를 아직 방문하지 않았다면
+        return False # 주의
+    # 현재 노드를 아직 방문하지 않았다면
     if graph[x][y] == 0:
-        #해당 노드 방문 처리
+        # 해당 노드 방문 처리
         graph[x][y] = 1
-        #상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
+        # 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
         dfs(x-1, y)
         dfs(x+1, y)
         dfs(x, y-1)
         dfs(x, y+1)
-        return True
-    return False
+        return True # 주의
+    return False # 주의
 
-#모든 노드에 대해 음료수 채우기
+# 모든 노드에 대해 음료수 채우기
 result = 0
 for i in range(n):
     for j in range(m):
-        if dfs(i,j):
-            result += 1 #한번에 만들 수 있는 아이스크림 개수 계산
+        if dfs(i, j): # 주의
+            result += 1 # 한번에 만들 수 있는 아이스크림 개수 계산
 
 print(result)
-
