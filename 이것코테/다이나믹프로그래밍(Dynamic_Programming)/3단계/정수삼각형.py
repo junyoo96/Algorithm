f14235bfe37c17187b07c9120758d74c52937f89
Youngjun Yoo <junyoo96@gmail.com>
2022-10-04 12:38:06 +0900

    Update 정수삼각형

--- 이것코테/다이나믹프로그래밍(Dynamic_Programming)/3단계/정수삼각형.py
+++ 이것코테/다이나믹프로그래밍(Dynamic_Programming)/3단계/정수삼각형.py
@@ -1,5 +1,10 @@
-# 답안
+# n : 삼각형 크기
+# 아래층으로 내려오면서 합 더하기
+# answer : 선택된 수의 합의 최대 합
 
+#================================================================
+# 답안코드
+#================================================================
 answer = 0
 n = int(input())
 dp = []
@@ -30,49 +35,45 @@ answer = max(dp[n-1])
 print(answer)
 
 # ==============================================================
-# 내가푼것
-# 9:06~9:19
-# 9:19~9:52(46/30)
+# 내 코드
+# ==============================================================
+# 9:06~9:19 / 9:19~9:52(46/30)
+# 10:17~10:33 / 10:33~10:51(34/30)
+
 # 다이나믹 프로그램-bottom up
+import sys
 
 answer = 0 # answer : 선택된 수의 최대 합
-n = int(input()) # n : 삼각형의 크기
-dp = [] # dp 테이블 : n*n 리스트 만들고 빈칸은 -1로 채우기
-
-# 삼각형 입력 받기
+n = int(sys.stdin.readline()) # n : 삼각형의 크기
+# n 크기의 dp 테이블 생성하면서 모두 -1로 채우기
+dp = [[-1] * n for _ in range(n)]
+# n만큼 반복하면서
 for i in range(n):
-    data = list(map(int, input().split()))
-    dp.append(data)
-    tmp = n - len(data)
-    for j in range(tmp):
-        dp[i].append(-1)
+    # 삼각형 입력 받기
+    tmp = list(map(int, sys.stdin.readline().split()))
+    for j in range(len(tmp)):
+        dp[i][j] = tmp[j]
 
 # 왼쪽 위, 바로 위에서만 올 수 있음
 dx = [-1, -1]
 dy = [-1, 0]
 
-# dp테이블 행 반복(1, )
+# dp테이블 행 1부터 반복하면서
 for i in range(1, n):
-    # dp 테이블 열 반복
+    # dp 테이블 열 반복하면서
     for j in range(n):
-        if dp[i][j] == -1:
-            continue
-        # 현재 칸으로 올 수 있는 두 방향 반복하면서
-        val_max = 0
-        for direction in range(2):
-            # val_max = 0
-            nx = i + dx[direction]
-            ny = j + dy[direction]
-            # 만약 방향이 dp테이블 범위안에 들어온다면
-            if 0 <= nx < n and 0 <= ny < n:
-                if dp[nx][ny] != -1:
-                    # val_max = max(val_max, 현재 값)
-                    val_max = max(val_max, dp[nx][ny])
-        # dp테이블 값 += val_max
-        dp[i][j] += val_max
+        if dp[i][j] != -1:
+            # 현재 위치 수 저장
+            value = dp[i][j]
+            # 2방향을 반복하면서
+            for d in range(2):
+                nx = i + dx[d]
+                ny = j + dy[d]
+                # 만약 현재 위치가 올바른 범위라면(범위안에 있고 -1이 아니라면)
+                if (0 <= nx < n and 0 <= ny < n) and dp[nx][ny] != -1:
+                    # 현재 위치수까지의 합 계산해서 최댓값 갱신
+                    dp[i][j] = max(dp[i][j], dp[nx][ny] + value)
 
 # dp테이블의 마지막 행에서 max값 찾기
-answer = max(dp[n - 1])  # 주의 - 마지막 행또는 마지막 열에서 최대값 찾을 때 for문 돌리지말고 행또는 열에 대한 리스트 뽑아서 max에 바로 넣기
-
+answer = max(dp[n - 1])  # 주의 - 마지막 행또는 마지막 열에서 최대값 찾을 때 for문 돌리지말고 행 또는 열에 대한 리스트 뽑아서 max에 바로 넣기
 print(answer)
-
