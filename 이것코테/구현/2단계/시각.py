fadf27052eb8b0fbbc1c7eb949c9271ab819c3c0
Youngjun Yoo <junyoo96@gmail.com>
2022-10-20 13:05:01 +0900

    Update 시각

--- 이것코테/구현/2단계/시각.py
+++ 이것코테/구현/2단계/시각.py
@@ -1,21 +1,13 @@
-#시각(p.113)
-
-#input : 5
-#output : 11475
+# answer : 3이 하나라도 포함된 모든 경우의 수
 
 #시간 입력받기 
 h = int(input())
-answer = 0 
 
-for i in range(h+1):
+answer = 0
+for i in range(h + 1):
   for j in range(60):
     for m in range(60):
-      if '3' in str(i)+str(j)+str(m): #매 시각 안에 '3' 이 포함되면 카운트 
+      if '3' in str(i) + str(j) + str(m): #매 시각 안에 '3' 이 포함되면 카운트
         answer += 1
         
 print(answer)
-
-
-
-
-        
