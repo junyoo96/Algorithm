9e5e44706af17ef127661b6e1c6186a122249892
Youngjun Yoo <junyoo96@gmail.com>
2022-10-24 14:04:59 +0900

    Update 럭키스트레이트

--- 이것코테/구현/3단계/럭키스트레이트.py
+++ 이것코테/구현/3단계/럭키스트레이트.py
@@ -1,16 +1,12 @@
 # #https://www.acmicpc.net/problem/18406
-# #3:45~ 4:04
-# # 자릿수 문제이기 때문에 리스트 형태로 받아서 풀기
-# a = list(map(int, input()))
-# digit = len(a) // 2
-# # 나뉜 자릿수 기준으로 각 자릿수 숫자들의 합이 동일하다면
-# if sum(a[:digit]) == sum(a[digit:]):
-#     print("LUCKY")
-# else:
-#     print("READY")
+# 3:45~4:04
+# 10:12~
 
-a = 4321
-a = list(map(int,str(a)))
-// [4, 3, 2, 1]
-print(len(a))
-// 4
+# 자릿수 문제이기 때문에 리스트 형태로 받아서 풀기
+a = list(map(int, input())) # 주의 - 문자열을 숫자 리스트로 변환 방법
+digit = len(a) // 2 # 주의 - / 연산자 사용하면 실수로 계산되는 것
+# 나뉜 자릿수 기준으로 각 자릿수 숫자들의 합이 동일하다면
+if sum(a[:digit]) == sum(a[digit:]):
+    print("LUCKY")
+else:
+    print("READY")
