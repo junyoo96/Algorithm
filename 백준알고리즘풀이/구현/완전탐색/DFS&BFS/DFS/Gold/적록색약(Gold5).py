b9ff9f4128f3fa112c251d8e7881fad4592f985d
Youngjun Yoo <junyoo96@gmail.com>
2023-02-14 17:27:43 +0900

    구현 복습

--- 백준알고리즘풀이/구현/완전탐색/DFS&BFS/DFS/Gold/적록색약(Gold5).py
+++ 백준알고리즘풀이/구현/완전탐색/DFS&BFS/DFS/Gold/적록색약(Gold5).py
@@ -1,11 +1,65 @@
 # 10:18~45 / 10:50~
+# 12:11~12:33/12:33~12:43
+
 # n : 그림 크기
 #     R , G ,B 색깔
 #     구역은 같은 색으로 이루어짐
 #         같은 색상이 상하좌우로 인접한 경우 같은 구역
 #         빨간색과 초록색이 인접해 있는 경우도 같은 색상
 # answer : 적록색약인 사람이 봤을 때와 아닌 사람이 봤을 때 구역의 수 구하기
+#====================================================================
+# 최적화 코드
+import sys
+sys.setrecursionlimit(100000)
+
+n = int(input())
+# 중요 - 사전에 색약용 맵을 따로 만들어 R과 G 동일하게 저장하기
+# 색약아닌 사람 맵이자 방문 여부 저장
+normal = [[0] * n for _ in range(n)]
+# 색약용 사람 맵이자 방문 여부 저장
+redgreen = [[0] * n for _ in range(n)]
+for i in range(n):
+    tmp = list(input())
+    for j in range(n):
+        normal[i][j] = tmp[j]
+        redgreen[i][j] = tmp[j]
+        # 색약용 맵에는 R과 G 동일한값으로 저장
+        if tmp[j] == 'G':
+            redgreen[i][j] = 'R'
+
+# 방향 그래프
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def dfs(x, y, arr):
+    current_color = arr[x][y]
+    arr[x][y] = 0
+
+    # 상하좌우 방향을 반복하면서
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        # 만약 이동할 좌표가 범위안에 있고 이동할 좌표를 방문한적이 없고 현재좌표의 색과 이동할 좌표의 색이 같다면
+        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] != 0 and current_color == arr[nx][ny]:
+            dfs(nx, ny, arr)
+
+# 적록색약 아닌 경우 카운트
+normal_count = 0
+# 적록색약인 경우 카운트
+redgreen_count = 0
+for i in range(n):
+    for j in range(n):
+        if normal[i][j] != 0:
+            dfs(i, j, normal)
+            normal_count += 1
+        if redgreen[i][j] != 0:
+            dfs(i, j, redgreen)
+            redgreen_count += 1
+
+print(normal_count, redgreen_count)
 # ===================================================================
+# 내 코드
 import sys
 sys.setrecursionlimit(100000)
 
--- 백준알고리즘풀이/구현/완전탐색/DFS&BFS/DFS/Silver/안전영역(Silver1).py
+++ 백준알고리즘풀이/구현/완전탐색/DFS&BFS/DFS/Silver/안전영역(Silver1).py
@@ -4,6 +4,62 @@
 # n : 행열 크기
 # 각 원소는 지점의 높이를 표시
 # answer : 물에 잠기지 않는 안정한 영역의 최대 개수
+#==========================================================
+# 개선한 코드(시간 단축, 1476ms)
+import sys
+sys.setrecursionlimit(100000) # 주의 - 파이썬에서는 1000번이상의 recursion 발생하면 recursion error가 발생하므로, recursion 제한을 더 늘려서 설정
+
+# answer 변수
+answer = 0
+# n 입력
+n = int(input())
+# 그래프 입력
+max_height = 1
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+    for j in range(n):
+        max_height = max(max_height, graph[i][j])
+
+# 상우하좌 리스트
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+# 함수 dfs(방문한 좌표, 그래프)
+def dfs(x, y, height):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        # 만약 방문한 좌표가 올바른 범위안에 있으면
+        if 0 <= nx < n and 0 <= ny < n:
+            # 비에 잠기지 높이보다 높고 아직 방문하지 않았다면
+            if graph[nx][ny] > height and visit[nx][ny] == 0:
+                visit[nx][ny] = 1
+                dfs(nx, ny, height)
+
+# 높이를 반복하면서
+for height in range(max_height): # 주의 - 문제에서 아무 지역도 물에 잠기지 않을 수 있다는 단서가 있으므로, 비가 아예 오지 않는 경우가 포함해야함
+    safe = 0
+    visit = [[0] * n for _ in range(n)] # 방문 여부 확인 그래프
+    # 그래프를 반복하면서
+    for i in range(n):
+        for j in range(n):
+            # 비에 잠기는 높이보다 높고 아직 방문하지 않았다면
+            # 내 코드와 다르게 dfs 호출하기 전에 비에 잠기는 높이인지와 방문 여부를 확인
+            if graph[i][j] > height and visit[i][j] == 0:
+                # 방문처리
+                visit[i][j] = 1
+                # 안전영역 개수 증가
+                safe += 1
+                # dfs 호출
+                dfs(i, j, height)
+
+    # 최대 안전영역 개수 계산
+    answer = max(answer, safe)
+
+# answer 출력
+print(answer)
 #=================================================================
 # 내가 작성한 코드(2004ms)
 #=================================================================
@@ -52,65 +108,6 @@ for height in range(max_height): # 주의 - 문제에서 아무 지역도 물에
             if dfs(i, j, height):
                 # 안전영역 증가
                 safe += 1
-
-    # 최대 안전영역 개수 계산
-    answer = max(answer, safe)
-
-# answer 출력
-print(answer)
-
-#==========================================================
-# 개선한 코드(시간 단축, 1476ms)
-#==========================================================
-import sys
-sys.setrecursionlimit(100000) # 주의 - 파이썬에서는 1000번이상의 recursion 발생하면 recursion error가 발생하므로, recursion 제한을 더 늘려서 설정
-import copy
-
-# answer 변수
-answer = 0
-# n 입력
-n = int(input())
-# 그래프 입력
-max_height = 1
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input().split())))
-    for j in range(n):
-        max_height = max(max_height, graph[i][j])
-
-# 상우하좌 리스트
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-# 함수 dfs(방문한 좌표, 그래프)
-def dfs(x, y, height):
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        # 만약 방문한 좌표가 올바른 범위안에 있으면
-        if 0 <= nx < n and 0 <= ny < n:
-            # 비에 잠기지 높이보다 높고 아직 방문하지 않았다면
-            if graph[nx][ny] > height and visit[nx][ny] == 0:
-                visit[nx][ny] = 1
-                dfs(nx, ny, height)
-
-# 높이를 반복하면서
-for height in range(max_height): # 주의 - 문제에서 아무 지역도 물에 잠기지 않을 수 있다는 단서가 있으므로, 비가 아예 오지 않는 경우가 포함해야함
-    safe = 0
-    visit = [[0] * n for _ in range(n)] # 방문 여부 확인 그래프
-    # 그래프를 반복하면서
-    for i in range(n):
-        for j in range(n):
-            # 비에 잠기는 높이보다 높고 아직 방문하지 않았다면
-            # 내 코드와 다르게 dfs 호출하기 전에 비에 잠기는 높이인지와 방문 여부를 확인
-            if graph[i][j] > height and visit[i][j] == 0:
-                # 방문처리
-                visit[i][j] = 1
-                # 안전영역 개수 증가
-                safe += 1
-                # dfs 호출
-                dfs(i, j, height)
 
     # 최대 안전영역 개수 계산
     answer = max(answer, safe)
