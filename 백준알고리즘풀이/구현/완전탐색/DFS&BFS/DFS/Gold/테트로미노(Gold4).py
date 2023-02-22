cf805c40d76bfaec27f5d8a7b3c5547afbc86843
Youngjun Yoo <junyoo96@gmail.com>
2023-02-22 13:01:03 +0900

    Update 테트로미노

--- 백준알고리즘풀이/구현/완전탐색/DFS&BFS/DFS/Gold/테트로미노(Gold4).py
+++ 백준알고리즘풀이/구현/완전탐색/DFS&BFS/DFS/Gold/테트로미노(Gold4).py
@@ -1,4 +1,5 @@
 # 9:09~9:30/9:30~ - 아이디어 생각안남
+# 10:00~ - ㅗ,ㅓ,ㅜ,ㅏ 만드는 아이디어 생각안남
 
 # 테트로미노(5가지)
 # n,m : 종이 칸(4~500)
@@ -26,11 +27,17 @@ answer = 0
 # ㅗ, ㅜ, ㅓ, ㅏ 제외한 모양들 최대값 계산
 # dfs의 경로모양이 ㅗ, ㅜ, ㅓ, ㅏ를 제외한 모든 모양들을 만들 수 있음
 def dfs(x, y, dsum, cnt):
+    # print("=========")
+    # print(x, y, cnt)
+    # print("방문리스트")
+    # for v in visited:
+    #     print(*v)
+    # print("=========")
     global answer
     # 중요(백트래킹 부분) - 만약 현재까지의 최대값이 현재 경로까지의 합 + 종이 수들 중 최대값 * 남은 경로 횟수보다 크다면
         # 남은 경로에서 종이수들중 최대값만 나와서 더해준다고 해도 현재 최대값보다 커지지 못하는 경우는 미리 가지치기
-    if answer >= dsum + max_val * (4 - cnt):
-        return
+    # if answer >= dsum + max_val * (4 - cnt):
+    #     return
     # 모양 완성되었을 때 최대값 계산
     if cnt == 4:
         answer = max(answer, dsum)
@@ -45,6 +52,7 @@ def dfs(x, y, dsum, cnt):
         if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
             # ㅗ, ㅜ, ㅓ, ㅏ 모양에 대한 처리
             if cnt == 2:
+                print("들어옴")
                 # 방문 처리
                 visited[nx][ny] = True
                 # dfs(이동좌표, 현재까지 경로의 수의 합, 지나온 칸 개수)
