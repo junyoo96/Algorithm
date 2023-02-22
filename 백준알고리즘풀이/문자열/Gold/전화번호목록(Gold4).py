65eb948c8e2c9e422c25bb674adb1734e45c211d
Youngjun Yoo <junyoo96@gmail.com>
2023-02-22 13:01:44 +0900

    Update 전화번호목록

--- 백준알고리즘풀이/문자열/Gold/전화번호목록(Gold4).py
+++ 백준알고리즘풀이/문자열/Gold/전화번호목록(Gold4).py
@@ -1,4 +1,6 @@
 # 11:33~ 아이디어 생각 못함
+# 12:33~ - rstrip안해서 틀림
+
 # 일관성
     # 한 번호가 다른 번호의 접두어인 경우가 없어야함
 # n : 전화번호 수(1~10000)
@@ -14,14 +16,13 @@ t = int(input().rstrip())
 # t 만큼 반복하면서
 for _ in range(t):
     # n 입력
-    n = int(input().rstrip())
+    n = int(input().rstrip()) # 주의 - readline으로 input받을 경우에는 rstrip 꼭 하기
     # n만큼 전화번호 목록 입력
     numbers = [input().rstrip() for _ in range(n)]
     # 중요 - 전화번호 목록 오름차순 정렬하면 pefix의 가능성이 있는 번호가 앞뒤로 정렬되므로 바로 뒤에 있는 번호만 확인하면 됨
     numbers.sort()
 
     flag = True
-
     answer = "YES"
     for i in range(n - 1):
         length = len(numbers[i])
